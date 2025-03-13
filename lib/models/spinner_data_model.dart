57cb887265aaa9edbae4c917de6abe068109af54
Hamed Zandieh <Hamed.Zandieh@gmail.com>
2025-03-13 21:34:52 +0330

    .

---
+++ lib/models/spinner_data_model.dart
@@ -0,0 +1,10 @@
+class SpinnerDataModel {
+  String name;
+  int id;
+  bool? isSelected;
+  dynamic data;
+
+  SpinnerDataModel(
+      {required this.name, required this.id, this.data, bool? isSelected})
+      : isSelected = isSelected ?? false;
+}
